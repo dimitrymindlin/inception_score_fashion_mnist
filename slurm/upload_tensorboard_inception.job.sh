39472d81eb2f6b2c0629ca48636ec5b080170fb1
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-26 12:24:58 +0000

    adapted script path.

--- slurm/upload_tensorboard_inception.job.sh
+++ slurm/upload_tensorboard_inception.job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /media/compute/homes/dmindlin/.bashrc
-conda activate medical-decision-support-system
+conda activate inception-score-fashion-mnist
 
-cd /media/compute/homes/dmindlin/medical-decision-support-system
+cd /media/compute/homes/dmindlin/inception_score_fashion_mnist
 
 tensorboard dev upload --logdir logs \
     --name "Inception FashionMnist" \
